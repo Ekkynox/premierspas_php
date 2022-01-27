830dc2570489e15e13a4a614e156312d108b389e
Clara <clara.louise.goncalves@gmail.com>
2022-01-27 10:23:31 +0100

    création d'une deuxième page

---
+++ includes/variables.php
@@ -0,0 +1,24 @@
+<?php
+    $myProducts = [
+        [
+            'name' => "Bonnet en laine",
+            'price' => 10,
+            'desc' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.",
+            'src' => "https://maison-joseph.fr/1198-thickbox_default/bonnet-laine-et-fourrure.jpg"
+        ], [
+            'name' => "Bonnet en bio",
+            'price' => 14,
+            'desc' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.",
+            'src' => "https://www.altermundi.com/395470-thickbox_default/bonnet-en-laine-bio-cotelee-leaf.jpg"
+        ], [
+            'name' => "Bonnet en laine et cachemire",
+            'price' => 20,
+            'desc' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.",
+            'src' => "https://m.media-amazon.com/images/I/71oJSr9TGOL._AC_UY445_.jpg"
+        ], [
+            'name' => "Bonnet arc-en-ciel",
+            'price' => 12,
+            'desc' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.",
+            'src' => "https://rainbow-blog.com/wp-content/uploads/2018/03/bonnet-tricot-arc-en-ciel-fleur-300x300.jpg"
+        ]
+    ];
--- index.php
+++ index.php
@@ -1,32 +1,16 @@
 <?php
-    include "variables.php";
-    include "functions.php";
+    include_once "includes/header.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bonnets</title>
-</head>
-<body>
-    <table>
-        <thead><tr>
-            <th>N°</th>
-            <th>Nom</th>
-            <th>Prix TVA</th>
-            <th>Prix TTC</th>
-            <th>Description</th>
-        </tr></thead>
-        <tbody>
-            <?php foreach ($myProducts as $key => $product) {
-            echo "<tr><td>" . $key . "</td>";
-            displayProduct($product);
-            echo "</tr>";
-            }?>
-        </tbody>
-    </table>
-</body>
-</html>
+<div class="d-flex">
+    <?php   
+    for ($i = 0; $i < 3; $i++) {
+        displayProductCard($myProducts[$i]);
+    } ?>
+</div>
+
+<a href="list.php"><button type="button" class="btn btn-light">Voir tous les produits</button></a>
+
+<?php
+    include_once "includes/footer.php";
+?>
