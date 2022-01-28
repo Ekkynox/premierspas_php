553f26683b35e09320796ecace9ccd23d064e16e
Clara <clara.louise.goncalves@gmail.com>
2022-01-28 15:57:56 +0100

    Corrections

--- pages/cart.php
+++ pages/cart.php
@@ -15,16 +15,16 @@ if (isset($_SESSION['cart']) && !(empty($_SESSION['cart']))) { ?>
         <thead>
             <tr>
                 <th class="p-2">Nom</th>
-                <th class="p-2">Prix TTC</th>
                 <th class="p-2">Quantité</th>
+                <th class="p-2">Prix TTC</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($_SESSION['cart'] as $key => $product) { ?>
                 <tr>
                     <td> <?= $myProducts[$key]['name'] ?> </td>
-                    <td> <?= $myProducts[$key]['price'] * $product ?> </td>
                     <td> <?= $product ?> </td>
+                    <td> <?= $myProducts[$key]['price'] * $product ?> </td>
                     <td class="p-2"> <a type="button" class="btn btn-light" href="?page=cart&key=<?= $key ?>&type=plus">+1</a>
                         <a type="button" class="btn btn-light" href="?page=cart&key=<?= $key ?>&type=moins">-1</a>
                     </td>
--- pages/list.php
+++ pages/list.php
@@ -1,27 +1,31 @@
 <?php
-    if(isset($_GET['key'])) {
-        if(isset($_SESSION['cart'][$_GET['key']])) {
-            $_SESSION['cart'][$_GET['key']] += 1;
-        } else {
-            $_SESSION['cart'][$_GET['key']] = 1;
-        }
+if (isset($_GET['key'])) {
+    if (isset($_SESSION['cart'][$_GET['key']])) {
+        $_SESSION['cart'][$_GET['key']] += 1;
+    } else {
+        $_SESSION['cart'][$_GET['key']] = 1;
     }
+    header('Location: ?page=list');
+}
 ?>
 
 <table>
-        <thead><tr>
+    <thead>
+        <tr>
             <th class="p-2">N°</th>
             <th class="p-2">Nom</th>
             <th class="p-2">Prix TVA</th>
             <th class="p-2">Prix TTC</th>
             <th class="p-2">Description</th>
-        </tr></thead>
-        <tbody>
-            <?php foreach ($myProducts as $key => $product) {?>
-            <tr><td> <?= $key ?> </td>
-            <?php displayProduct($product) ?>
-            <td class="p-2"> <a type="button" class="btn btn-light" href="?page=list&key=<?= $key ?>">Ajouter au panier</a></td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($myProducts as $key => $product) { ?>
+            <tr>
+                <td> <?= $key ?> </td>
+                <?php displayProduct($product) ?>
+                <td class="p-2"> <a type="button" class="btn btn-light" href="?page=list&key=<?= $key ?>">Ajouter au panier</a></td>
             </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+        <?php } ?>
+    </tbody>
+</table>
