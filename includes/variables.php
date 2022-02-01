98a9d0ce5880a1badf74cd8b1e1a982169bcf5fe
Clara <clara.louise.goncalves@gmail.com>
2022-02-01 11:42:44 +0100

    création des filtres

--- includes/variables.php
+++ includes/variables.php
@@ -6,31 +6,31 @@ $myProducts = [
         10,
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.",
         "https://maison-joseph.fr/1198-thickbox_default/bonnet-laine-et-fourrure.jpg",
-        "laine",
-        "M"
+        ["Laine"],
+        ["M"]
     ),
     new Beanie(
         "Bonnet en bio",
         14,
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.",
         "https://www.altermundi.com/395470-thickbox_default/bonnet-en-laine-bio-cotelee-leaf.jpg",
-        "laine",
-        "S"
+        ["Laine"],
+        ["S"]
     ),
     new Beanie(
         "Bonnet en cachemire",
         20,
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.",
         "https://m.media-amazon.com/images/I/71oJSr9TGOL._AC_UY445_.jpg",
-        "cachemire",
-        "M"
+        ["Cachemire"],
+        ["M"]
     ),
     new Beanie(
         "Bonnet arc-en-ciel",
         12,
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.",
         "https://rainbow-blog.com/wp-content/uploads/2018/03/bonnet-tricot-arc-en-ciel-fleur-300x300.jpg",
-        "laine",
-        "S"
+        ["Laine"],
+        ["S"]
     )
 ];
--- pages/cart.php
+++ pages/cart.php
@@ -2,10 +2,10 @@
     if (isset($_SESSION['cart'][$_GET['key']])) {
         if ($_GET['type'] == 'moins') {
             $_SESSION['cart'][$_GET["key"]] -= 1;
-        } else if ($_GET['type'] == 'plus') {
+        } elseif ($_GET['type'] == 'plus') {
             $_SESSION['cart'][$_GET["key"]] += 1;
         }
-        if ($_SESSION['cart'][$_GET["key"]] == 0) {
+        if ($_SESSION['cart'][$_GET["key"]] <= 0) {
             unset($_SESSION['cart'][$_GET["key"]]);
         }
     } else {
@@ -24,7 +24,10 @@ if (isset($_SESSION['cart']) && !(empty($_SESSION['cart']))) { ?>
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($_SESSION['cart'] as $key => $quantity) { ?>
+            <?php
+            $prixTot = 0;
+            foreach ($_SESSION['cart'] as $key => $quantity) {
+                $prixTot += $myProducts[$key]->getPrice() * $quantity; ?>
                 <tr>
                     <td class="p-2"> <?= $myProducts[$key]->getName() ?> </td>
                     <td class="p-2"> <?= $quantity ?> </td>
@@ -33,10 +36,12 @@ if (isset($_SESSION['cart']) && !(empty($_SESSION['cart']))) { ?>
                         <a type="button" class="btn btn-light" href="?page=cart&key=<?= $key ?>&type=moins">-1</a>
                     </td>
                 </tr>
-            <?php } ?>
+            <?php
+            } ?>
         </tbody>
     </table>
-
-<?php } else { ?>
+    <p>Prix total : <?= number_format($prixTot, 2) ?> €</p>
+<?php
+} else { ?>
     <p class="m-5">Votre panier est vide.</p>
 <?php } ?>
